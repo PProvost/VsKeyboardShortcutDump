a8839358c3e0d461af170ccc6988c36db2d828b9
David Starr <david@elegantcode.com>
2012-12-20 10:41:47 -0800

    VS 2012 Support
    Updated references and project runtime to work with VS 2012 Update 1

---
+++ Backup/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace PeterProvost.VsKeyboardShortcutsDump
+{
+    static class GuidList
+    {
+        public const string guidVsKeyboardShortcutsDumpPkgString = "5933efec-981b-4b12-ace8-72a012aae2a9";
+        public const string guidVsKeyboardShortcutsDumpCmdSetString = "e5d2e960-3668-4dac-9a9a-cda83fee8f0e";
+
+        public static readonly Guid guidVsKeyboardShortcutsDumpCmdSet = new Guid(guidVsKeyboardShortcutsDumpCmdSetString);
+    };
+}
---
+++ Backup/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace PeterProvost.VsKeyboardShortcutsDump
+{
+    static class PkgCmdIDList
+    {
+        public const uint cmdidVsKeyboardShortcutDump =        0x100;
+
+
+    };
+}
